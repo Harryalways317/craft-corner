dedb33aebad6f5377d7dff7fc2aff467c7cbe848
Harish Vadaparty <harishvadaparty@gmail.com>
2023-04-25 16:53:57 +0530

    added navigation

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,27 @@
+import React, { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+import {ReactComponent as CraftCornerLogo} from "../../assets/crown.svg"
+import "./navigation.styles.scss"
+
+const Navigation = () => {
+    return (
+        <Fragment>
+           
+           <div className="navigation">
+           <Link className="logo-container" to='/'>
+           <div><CraftCornerLogo className="logo"/></div>
+           </Link>
+           <div className="nav-links-container">
+
+            <Link className="nav-link" to="/shop">
+                SHOP
+            </Link>
+           </div>
+           </div>
+               <Outlet />
+        </Fragment>
+     
+    );
+}
+
+export default Navigation;
